dba660218f4f1e0b81bc4e1c4f9e090ccfa6b7be
d.barwikowski <d.barwikowski@gmail.com>
2017-09-28 19:42:01 +0200

    More tests

---
+++ tests/Valit.Tests/TypeTests/decimal_tests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Xunit;
+
+namespace Valit.Tests.TypeTests
+{
+    public class decimal_tests
+    {
+        [Fact]
+        public void should_pass_for_decimal()
+        {
+            var result = ValitRules<object>
+                .For(0)
+                .WithStrategy(ValitRulesStrategies.Complete)
+                .Ensure(_ => Decimal.Parse("0"), _ => _
+                    .IsGreaterThan(Decimal.MinValue)
+                    .IsLessThan(Decimal.MaxValue)
+                    .IsEqualTo(Decimal.Parse("0")))
+                .Validate();
+
+            Assert.True(result.Succeeded);
+        }
+
+        [Fact]
+        public void should_not_pass_for_decimal()
+        {
+            var result = ValitRules<object>
+                .For(0)
+                .WithStrategy(ValitRulesStrategies.Complete)
+                .Ensure(_ => Decimal.Parse("0"), _ => _
+                    .IsGreaterThan(Decimal.Parse("1"))
+                    .WithMessage("Not greater than 1")
+                    .IsLessThan(Decimal.Parse("-1"))
+                    .WithMessage("Not less than -1"))
+                .Validate();
+
+            Assert.False(result.Succeeded);
+            Assert.Equal(2, result.Errors.Length);
+        }
+    }
+}
