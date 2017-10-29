7c2e9b5f4db277d6fbb204e7e2e6fe87f2b55267
GooRiOn <foreverframenet@gmail.com>
2017-10-29 17:30:57 +0100

    Added unit tests for DateTimeOffset type

--- tests/Valit.Tests/HelperExtensions/DateTimeOffsetExtensions.cs
+++ tests/Valit.Tests/HelperExtensions/DateTimeOffsetExtensions.cs
@@ -4,6 +4,9 @@ namespace Valit.Tests.HelperExtensions
 {
     public static class DateTimeOffsetExtensions
     {
+        public static DateTimeOffset AsDateTimeOffset(this string stringValue)
+            => Convert.ToDateTime(stringValue);
+            
         public static DateTimeOffset? AsNullableDateTimeOffset(this string stringValue)
             => string.IsNullOrWhiteSpace(stringValue)? (DateTimeOffset?)null : Convert.ToDateTime(stringValue);
     }
