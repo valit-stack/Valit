b5d77a4225dfc7a46e6587969cd5645512893614
GooRiOn <foreverframenet@gmail.com>
2017-11-01 20:00:27 +0100

    Added basic tests for tags

--- tests/Valit.Tests/Property/Property_Tag_Tests.cs
+++ tests/Valit.Tests/Property/Property_Tag_Tests.cs
@@ -25,6 +25,42 @@ namespace Valit.Tests.Property
             rules.Count().ShouldBe(3);
         }
 
+        [Fact]
+        public void ValitRules_GetTaggedRules_Returns_Correct_Number_Of_Rules()
+        {
+            var rules = ValitRules<Model>
+                .Create()
+                .Ensure(m => m.Value1, _=>_
+                    .Email()
+                    .Tag("Tag1"))
+                .Ensure(m => m.Value2, _=>_
+                    .Email())
+                .Ensure(m => m.Value2, _=>_
+                    .Required()
+                    .Tag("Tag2"))
+                .GetTaggedRules();
+
+            rules.Count().ShouldBe(2);
+        }
+
+        [Fact]
+        public void ValitRules_GetUntaggedRules_Returns_Correct_Number_Of_Rules()
+        {
+            var rules = ValitRules<Model>
+                .Create()
+                .Ensure(m => m.Value1, _=>_
+                    .Email()
+                    .Tag("Tag1"))
+                .Ensure(m => m.Value2, _=>_
+                    .Email())
+                .Ensure(m => m.Value2, _=>_
+                    .Required()
+                    .Tag("Tag2"))
+                .GetUntaggedRules();
+
+            rules.Count().ShouldBe(1);
+        }
+
 
         private Model _model => new Model();
 
