a81475a6adfd902158fa14daee6f53f23f2172fe
d.barwikowski <d.barwikowski@gmail.com>
2017-11-01 21:38:44 +0100

    Update

--- src/Valit/Rules/ValitRuleAccessorExtensions.cs
+++ src/Valit/Rules/ValitRuleAccessorExtensions.cs
@@ -1,15 +1,13 @@
-﻿namespace Valit.Rules
+﻿using Valit.Exceptions;
+
+namespace Valit.Rules
 {
     internal static class ValitRuleAccessorExtensions
     {
         internal static IValitRuleAccessor<TObject, TProperty> GetAccessor<TObject, TProperty>(this IValitRule<TObject, TProperty> rule) where TObject : class
         {
             var accessor = rule as IValitRuleAccessor<TObject, TProperty>;
-
-            if (accessor == null)
-            {
-                throw new ValitException("Rule doesn't have an accessor");
-            }
+            accessor.ThrowIfNull("Rule doesn't have an accessor");
 
             return accessor;
         }        
