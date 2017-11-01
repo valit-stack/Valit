e3be2c6d31289a5d11c69e8a9482600e3a056ffb
GooRiOn <foreverframenet@gmail.com>
2017-11-01 21:03:08 +0100

    Added missing unit test for tags

--- tests/Valit.Tests/Property/Property_Tag_Tests.cs
+++ tests/Valit.Tests/Property/Property_Tag_Tests.cs
@@ -6,7 +6,34 @@ namespace Valit.Tests.Property
 {
     public class Property_Tag_Tests
     {
-        
+    
+        [Fact]
+        public void Property_Tag_Throws_When_Null_Rule_Is_Given()
+        {
+            var exception = Record.Exception(() => {
+                ((IValitRule<Model, object>)null)
+                    .Tag("");
+            });
+
+            exception.ShouldBeOfType(typeof(ValitException));
+        }
+
+        [Fact]
+        public void Property_Tag_Throws_When_Null_Tags_Set_Is_Given()
+        {
+            var exception = Record.Exception(() => {
+                ValitRules<Model>
+                    .Create()
+                    .Ensure(m => m.Value1, _=>_
+                        .Tag(null))
+                    .For(_model)
+                    .Validate();
+            });
+
+            exception.ShouldBeOfType(typeof(ValitException));
+        }
+
+
         [Fact]
         public void ValitRules_GetAllRules_Returns_Correct_Number_Of_Rules()
         {
@@ -62,10 +89,13 @@ namespace Valit.Tests.Property
         }
 
         [Theory]
-        [InlineData("Tag1", true)]
-        [InlineData("Tag2", true)]      
-        [InlineData("Tag3", false)]      
-        public void ValitRules_Validate_With_Params_Should_Return_Proper_Result_Based_On_Given_Tags_Set(string tag, bool expected)
+        [InlineData(new [] { "Tag1" }, true)]
+        [InlineData(new [] { "Tag2" }, true)]      
+        [InlineData(new [] { "Tag1", "Tag2" }, true)]  
+        [InlineData(new [] { "Tag1", "Tag3" }, false)]  
+        [InlineData(new [] { "Tag2", "Tag3" }, false)]  
+        [InlineData(new [] { "Tag3" }, false)]        
+        public void ValitRules_Validate_With_Params_Should_Return_Proper_Result_Based_On_Given_Tags_Set(string[] tags, bool expected)
         {
             var result = ValitRules<Model>
                 .Create()
@@ -79,9 +109,57 @@ namespace Valit.Tests.Property
                     .Required()
                     .Tag("Tag3"))
                 .For(_model)
-                .Validate(new []{ tag });
+                .Validate(tags);
 
             Assert.Equal(result.Succeeded, expected);
+        }
+
+        [Theory]
+        [InlineData(new [] { "Tag1" }, true)]
+        [InlineData(new [] { "Tag2" }, true)]      
+        [InlineData(new [] { "Tag1", "Tag2" }, true)]  
+        [InlineData(new [] { "Tag1", "Tag3" }, false)]  
+        [InlineData(new [] { "Tag2", "Tag3" }, false)]  
+        [InlineData(new [] { "Tag3" }, false)]        
+        public void ValitRules_Validate_With_Predicate_Should_Return_Proper_Result_Based_On_Given_Tags_Set(string[] tags, bool expected)
+        {
+            var result = ValitRules<Model>
+                .Create()
+                .Ensure(m => m.Value1, _=>_
+                    .Required()
+                    .Tag("Tag1"))
+                .Ensure(m => m.Value2, _=>_
+                    .Required()
+                    .Tag("Tag2"))
+                .Ensure(m => m.NullValue, _=>_
+                    .Required()
+                    .Tag("Tag3"))
+                .For(_model)
+                .Validate(r => r.Tags.Intersect(tags).Any());
+
+            Assert.Equal(result.Succeeded, expected);
+        }
+
+        [Fact]
+        public void ValitRules_Create_Should_Properly_Add_Given_Rules()
+        {
+            var rules = ValitRules<Model>
+                .Create()
+                .Ensure(m => m.Value1, _=>_
+                    .Email()
+                    .Tag("Tag1"))
+                .Ensure(m => m.Value2, _=>_
+                    .Email())
+                .Ensure(m => m.NullValue, _=>_
+                    .Required()
+                    .Tag("Tag2"))
+                .GetAllRules();
+
+            var newRules = ValitRules<Model>
+                .Create(rules)
+                .GetAllRules(); 
+
+            newRules.Count().ShouldBe(3);
         }
 
 
