9994ae9697be54247b2e2a092bb62af14cb797eb
GooRiOn <foreverframenet@gmail.com>
2017-11-23 22:12:22 +0100

    Added first version of collection valit rule

---
+++ src/Valit/Rules/CollectionValitRule.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Valit.Result;
+
+namespace Valit.Rules
+{
+	public class CollectionValitRule<TObject, TProperty> : IValitRule<TObject> where TObject : class where TProperty : class
+	{
+		IEnumerable<string> IValitRule.Tags => Enumerable.Empty<string>();
+
+        private readonly Func<TObject, IEnumerable<TProperty>> _collectionSelector;
+        private readonly Func<IValitRule<TObject, TProperty>,IValitRule<TObject, TProperty>> _ruleFunc;
+        private readonly IValitStrategy _strategy;
+		private readonly IValitMessageProvider _messageProvider;
+
+        public CollectionValitRule(
+			Func<TObject, IEnumerable<TProperty>> collectionSelector, 
+			Func<IValitRule<TObject, TProperty>,IValitRule<TObject, TProperty>> ruleFunc, 
+			IValitStrategy strategy,
+			IValitMessageProvider messageProvider)
+        {
+            _collectionSelector = collectionSelector;
+			_ruleFunc = ruleFunc;
+            _strategy = strategy;
+			_messageProvider = messageProvider;
+        }
+
+		IValitResult IValitRule<TObject>.Validate(TObject @object)
+		{
+			var collection = _collectionSelector(@object);
+
+			var result = ValitResult.Success;
+
+			foreach(var property in collection)
+			{
+				Func<TObject, TProperty> selector = @obj => property;
+				var lastEnsureRule = _ruleFunc(new ValitRule<TObject, TProperty>(selector, _messageProvider));
+            	var propertyRules = lastEnsureRule.GetAllEnsureRules();
+
+				result &= ValitRules<TObject>
+					.Create(propertyRules)
+					.WithStrategy(_strategy)
+					.For(@object)
+					.Validate();
+
+                if(!result.Succeeded)
+                {
+                    _strategy.Fail(default(IValitRule<TObject>), result, out bool cancel);
+                    if(cancel)
+                    {
+                        break;
+                    }
+                }
+			}
+
+			return result;
+		}
+	}
+}
