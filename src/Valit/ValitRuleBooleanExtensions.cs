377c5db7c09865271665c9aa49dd30e7569ca5fa
tim <timdeschryver@outlook.com>
2017-11-26 18:34:03 +0100

    add required

--- src/Valit/ValitRuleBooleanExtensions.cs
+++ src/Valit/ValitRuleBooleanExtensions.cs
@@ -25,5 +25,8 @@ namespace Valit
 
     public static IValitRule<TObject, bool?> IsEqualTo<TObject>(this IValitRule<TObject, bool?> rule, bool? value) where TObject : class
         => rule.Satisfies(p => p.HasValue && value.HasValue && p.Value == value.Value);
+
+    public static IValitRule<TObject, bool?> Required<TObject>(this IValitRule<TObject, bool?> rule) where TObject : class
+        => rule.Satisfies(p => p.HasValue);
   }
 }
