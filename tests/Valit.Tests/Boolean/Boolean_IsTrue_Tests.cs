6f5ff36767356802052c2b4f9d27da90cacae13a
tim <timdeschryver@outlook.com>
2017-11-27 12:39:44 +0100

    fix indention similar to other files

--- tests/Valit.Tests/Boolean/Boolean_IsTrue_Tests.cs
+++ tests/Valit.Tests/Boolean/Boolean_IsTrue_Tests.cs
@@ -6,90 +6,90 @@ namespace Valit.Tests.Boolean
 {
   public class Boolean_IsTrue_Tests
   {
-    [Fact]
-    public void Boolean_IsTrue_For_Not_Nullable_Value_Throws_When_Null_Rule_Is_Given()
-    {
-      var exception = Record.Exception(() =>
-      {
-        ((IValitRule<Model, bool>)null)
-            .IsTrue();
-      });
+        [Fact]
+        public void Boolean_IsTrue_For_Not_Nullable_Value_Throws_When_Null_Rule_Is_Given()
+        {
+            var exception = Record.Exception(() =>
+            {
+                ((IValitRule<Model, bool>)null)
+                    .IsTrue();
+            });
 
-      exception.ShouldBeOfType(typeof(ValitException));
-    }
+            exception.ShouldBeOfType(typeof(ValitException));
+        }
 
-    [Fact]
-    public void Boolean_IsTrue_For_Nullable_Value_Throws_When_Null_Rule_Is_Given()
-    {
-      var exception = Record.Exception(() =>
-      {
-        ((IValitRule<Model, bool?>)null)
-            .IsTrue();
-      });
+        [Fact]
+        public void Boolean_IsTrue_For_Nullable_Value_Throws_When_Null_Rule_Is_Given()
+        {
+            var exception = Record.Exception(() =>
+            {
+                ((IValitRule<Model, bool?>)null)
+                    .IsTrue();
+            });
 
-      exception.ShouldBeOfType(typeof(ValitException));
-    }
+            exception.ShouldBeOfType(typeof(ValitException));
+        }
 
-    [Theory]
-    [InlineData(true, true)]
-    [InlineData(false, false)]
-    public void Boolean_IsTrue_Returns_Proper_Results_For_Not_Nullable_Values(bool useTrueValue, bool expected)
-    {
-      IValitResult result = ValitRules<Model>
-          .Create()
-          .Ensure(m => useTrueValue ? m.TrueValue : m.FalseValue, _ => _
-              .IsTrue())
-          .For(_model)
-          .Validate();
+        [Theory]
+        [InlineData(true, true)]
+        [InlineData(false, false)]
+        public void Boolean_IsTrue_Returns_Proper_Results_For_Not_Nullable_Values(bool useTrueValue, bool expected)
+        {
+            IValitResult result = ValitRules<Model>
+                .Create()
+                .Ensure(m => useTrueValue ? m.TrueValue : m.FalseValue, _ => _
+                    .IsTrue())
+                .For(_model)
+                .Validate();
 
-      Assert.Equal(result.Succeeded, expected);
-    }
+            Assert.Equal(result.Succeeded, expected);
+        }
 
-    [Theory]
-    [InlineData(true, true)]
-    [InlineData(false, false)]
-    public void Boolean_IsTrue_Returns_Proper_Results_For_Nullable_Values(bool useTrueValue, bool expected)
-    {
-      IValitResult result = ValitRules<Model>
-          .Create()
-          .Ensure(m => useTrueValue ? m.NullableTrueValue : m.NullableFalseValue, _ => _
-              .IsTrue())
-          .For(_model)
-          .Validate();
+        [Theory]
+        [InlineData(true, true)]
+        [InlineData(false, false)]
+        public void Boolean_IsTrue_Returns_Proper_Results_For_Nullable_Values(bool useTrueValue, bool expected)
+        {
+            IValitResult result = ValitRules<Model>
+                .Create()
+                .Ensure(m => useTrueValue ? m.NullableTrueValue : m.NullableFalseValue, _ => _
+                    .IsTrue())
+                .For(_model)
+                .Validate();
 
-      Assert.Equal(result.Succeeded, expected);
-    }
+            Assert.Equal(result.Succeeded, expected);
+        }
 
-    [Fact]
-    public void Boolean_IsTrue_Fails_For_Null_Value()
-    {
-      IValitResult result = ValitRules<Model>
-          .Create()
-          .Ensure(m => m.NullValue, _ => _
-              .IsTrue())
-          .For(_model)
-          .Validate();
+        [Fact]
+        public void Boolean_IsTrue_Fails_For_Null_Value()
+        {
+            IValitResult result = ValitRules<Model>
+                .Create()
+                .Ensure(m => m.NullValue, _ => _
+                    .IsTrue())
+                .For(_model)
+                .Validate();
 
-      Assert.Equal(result.Succeeded, false);
-    }
+            Assert.Equal(result.Succeeded, false);
+        }
 
 
-    #region ARRANGE
-    public Boolean_IsTrue_Tests()
-    {
-      _model = new Model();
-    }
+        #region ARRANGE
+        public Boolean_IsTrue_Tests()
+        {
+            _model = new Model();
+        }
 
-    private readonly Model _model;
+        private readonly Model _model;
 
-    class Model
-    {
-      public bool TrueValue => true;
-      public bool FalseValue => false;
-      public bool? NullableTrueValue => true;
-      public bool? NullableFalseValue => false;
-      public bool? NullValue => null;
-    }
-    #endregion
+        class Model
+        {
+            public bool TrueValue => true;
+            public bool FalseValue => false;
+            public bool? NullableTrueValue => true;
+            public bool? NullableFalseValue => false;
+            public bool? NullValue => null;
+        }
+        #endregion
   }
 }
--- tests/Valit.Tests/Boolean/Boolean_Required_Tests.cs
+++ tests/Valit.Tests/Boolean/Boolean_Required_Tests.cs
@@ -3,50 +3,49 @@ using Xunit;
 
 namespace Valit.Tests.Boolean
 {
-  public class Boolean_Required_Tests
-  {
-
-    [Fact]
-    public void Boolean_Required_For_Nullable_Value_Throws_When_Null_Rule_Is_Given()
+    public class Boolean_Required_Tests
     {
-      var exception = Record.Exception(() =>
-      {
-        ((IValitRule<Model, bool?>)null)
-            .Required();
-      });
+        [Fact]
+        public void Boolean_Required_For_Nullable_Value_Throws_When_Null_Rule_Is_Given()
+        {
+            var exception = Record.Exception(() =>
+            {
+                ((IValitRule<Model, bool?>)null)
+                    .Required();
+            });
 
-      exception.ShouldBeOfType(typeof(ValitException));
+            exception.ShouldBeOfType(typeof(ValitException));
+        }
+
+
+        [Theory]
+        [InlineData(false, true)]
+        [InlineData(true, false)]
+        public void Boolean_Required_Returns_Proper_Results_For_Nullable_Value(bool useNullValue, bool expected)
+        {
+            IValitResult result = ValitRules<Model>
+                .Create()
+                .Ensure(m => useNullValue ? m.NullValue : m.NullableValue, _ => _
+                    .Required())
+                .For(_model)
+                .Validate();
+
+            Assert.Equal(result.Succeeded, expected);
+        }
+
+        #region ARRANGE
+        public Boolean_Required_Tests()
+        {
+            _model = new Model();
+        }
+
+        private readonly Model _model;
+
+        class Model
+        {
+            public bool? NullableValue => true;
+            public bool? NullValue => null;
+        }
+        #endregion
     }
-
-
-    [Theory]
-    [InlineData(false, true)]
-    [InlineData(true, false)]
-    public void Boolean_Required_Returns_Proper_Results_For_Nullable_Value(bool useNullValue, bool expected)
-    {
-      IValitResult result = ValitRules<Model>
-          .Create()
-          .Ensure(m => useNullValue ? m.NullValue : m.NullableValue, _ => _
-               .Required())
-          .For(_model)
-          .Validate();
-
-      Assert.Equal(result.Succeeded, expected);
-    }
-
-    #region ARRANGE
-    public Boolean_Required_Tests()
-    {
-      _model = new Model();
-    }
-
-    private readonly Model _model;
-
-    class Model
-    {
-      public bool? NullableValue => true;
-      public bool? NullValue => null;
-    }
-    #endregion
-  }
 }
