f8eeec3945762bdc6122a4a7fe9e293e6989e6a0
Magnus Toftdal <duckhero@gmail.com>
2017-11-28 09:28:01 +0100

    Minor stylechange
    Added Func as a suffix for the Func<string>

--- src/Valit/Errors/ValitRuleError.cs
+++ src/Valit/Errors/ValitRuleError.cs
@@ -4,23 +4,23 @@ namespace Valit.Errors
 {
     internal class ValitRuleError
     {
-        private readonly Func<string> _message;
-        public string Message => _message();
+        private readonly Func<string> _messageFunc;
+        public string Message => _messageFunc();
         public int? ErrorCode { get; }
 
-        private ValitRuleError(Func<string> message)
+        private ValitRuleError(Func<string> messageFunc)
         {
-            _message = message;
+            _messageFunc = messageFunc;
         }
 
         private ValitRuleError(int errorCode)
         {
             ErrorCode = errorCode;
-            _message = () => null;
+            _messageFunc = () => null;
         }
 
-        public static ValitRuleError CreateForMessage(Func<string> message)
-            => new ValitRuleError(message);
+        public static ValitRuleError CreateForMessage(Func<string> messageFunc)
+            => new ValitRuleError(messageFunc);
 
         public static ValitRuleError CreateForErrorCode(int errorCode)
             => new ValitRuleError(errorCode);   
