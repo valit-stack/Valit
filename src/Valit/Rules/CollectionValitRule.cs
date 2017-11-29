421eeeb2e5c872023dcba7862eef38b9d5bab136
GooRiOn <foreverframenet@gmail.com>
2017-11-29 21:26:06 +0100

    Made Tags public in all IValitRule implementations just for a consistency

--- src/Valit/Rules/CollectionValitRule.cs
+++ src/Valit/Rules/CollectionValitRule.cs
@@ -8,7 +8,7 @@ namespace Valit.Rules
 {
 	internal class CollectionValitRule<TObject, TProperty> : IValitRule<TObject> where TObject : class
 	{
-		IEnumerable<string> IValitRule.Tags => Enumerable.Empty<string>();
+		public IEnumerable<string> Tags { get; private set; } 
 
         private readonly Func<TObject, IEnumerable<TProperty>> _collectionSelector;
         private readonly Func<IValitRule<TObject, TProperty>,IValitRule<TObject, TProperty>> _ruleFunc;
@@ -21,6 +21,7 @@ namespace Valit.Rules
 			IValitStrategy strategy,
 			IValitMessageProvider messageProvider)
         {
+			Tags = Enumerable.Empty<string>();
             _collectionSelector = collectionSelector;
 			_ruleFunc = ruleFunc;
             _strategy = strategy;
