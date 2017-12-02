efd93888aeb913536b90a377625553f80d2f36db
GooRiOn <foreverframenet@gmail.com>
2017-12-02 23:08:53 +0100

    Added CreateValitator extension method for IValitRules

--- tests/Valit.Tests/Valitator/Valitator_ValitRules_Tests.cs
+++ tests/Valit.Tests/Valitator/Valitator_ValitRules_Tests.cs
@@ -4,13 +4,13 @@ using Xunit;
 
 namespace Valit.Tests.Valitator
 {
-    public class Valitator_Tests
+    public class Valitator_ValitRules_Tests
     {
         [Fact]
         public void CreateValitator_Throws_When_Null_ValitRulesProvider_Is_Given()
         {
             var exception = Record.Exception(() => {
-                var valitator = ((IValitRulesProvider<Model>)null).CreateValitator();
+                var valitator = ((IValitRules<Model>)null).CreateValitator();
             });
 
             exception.ShouldBeOfType(typeof(ValitException));
@@ -19,7 +19,7 @@ namespace Valit.Tests.Valitator
         [Fact]
         public void Created_Valitator_Properly_Handles_Complete_Strategy()
         {
-            var valitator = new ModelRulesProvider().CreateValitator();
+            var valitator = _valitRules.CreateValitator();
             var result = valitator.Validate(_model);
 
             Assert.False(result.Succeeded);
@@ -31,7 +31,7 @@ namespace Valit.Tests.Valitator
         [Fact]
         public void Created_Valitator_Properly_Handles_FailFast_Strategy()
         {
-            var valitator = new ModelRulesProvider().CreateValitator();
+            var valitator = _valitRules.CreateValitator();
             var result = valitator.Validate(_model, new FailFastValitStrategy());
 
             Assert.False(result.Succeeded);
@@ -49,31 +49,27 @@ namespace Valit.Tests.Valitator
             public string StringValue { get; set; }
         }
 
-        class ModelRulesProvider : IValitRulesProvider<Model>
-        {
-            public IEnumerable<IValitRule<Model>> GetRules()
-                => ValitRules<Model>
-                        .Create()
-                        .Ensure(m => m.NumericValue, _=>_
-                            .IsGreaterThan(10)
-                                .WithMessage("One"))
-                        .Ensure(m => m.StringValue, _=>_
-                            .Required()
-                                .WithMessage("Two")
-                            .Email()
-                                .WithMessage("Three"))
-                        .GetAllRules();
-        }
-
         private readonly Model _model;
+        private readonly IValitRules<Model> _valitRules;
 
-        public Valitator_Tests()
+        public Valitator_ValitRules_Tests()
         {
             _model = new Model
             {
                 NumericValue = 3,
                 StringValue = "Text"
             };
+
+            _valitRules = ValitRules<Model>
+                .Create()
+                .Ensure(m => m.NumericValue, _=>_
+                    .IsGreaterThan(10)
+                        .WithMessage("One"))
+                .Ensure(m => m.StringValue, _=>_
+                    .Required()
+                        .WithMessage("Two")
+                    .Email()
+                        .WithMessage("Three"));
         }
     }
 #endregion
