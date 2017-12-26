94ab90e46a6db6b648598555d93048569b11efe0
Dariusz Pawlukiewicz <foreverframenet@gmail.com>
2017-12-26 17:35:02 +0100

    Changed all selectors to Expressions

--- src/Valit/Rules/NestedObjectValitRule.cs
+++ src/Valit/Rules/NestedObjectValitRule.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using Valit.Exceptions;
 
 namespace Valit.Rules
@@ -8,17 +9,17 @@ namespace Valit.Rules
     internal class NestedObjectValitRule<TObject, TProperty> : IValitRule<TObject> where TObject : class where TProperty : class
     {
         public IEnumerable<string> Tags { get; set; }
-        private readonly Func<TObject, TProperty> _propertySelector;
+        private readonly Expression<Func<TObject, TProperty>> _propertySelector;
         private readonly IValitator<TProperty> _valitator;
         private readonly IValitStrategy _strategy;
 
         public NestedObjectValitRule(
-            Func<TObject, TProperty> selector,
+            Expression<Func<TObject, TProperty>> propertySelector,
             IValitator<TProperty> valitator,
             IValitStrategy strategy)
         {
             Tags = Enumerable.Empty<string>();
-            _propertySelector = selector;
+            _propertySelector = propertySelector;
             _valitator = valitator;
             _strategy = strategy;
         }
@@ -27,7 +28,7 @@ namespace Valit.Rules
         {
             @object.ThrowIfNull();
 
-            var property = _propertySelector(@object);
+            var property = _propertySelector.Compile().Invoke(@object);
             return _valitator.Validate(property, _strategy);
         }
     }
