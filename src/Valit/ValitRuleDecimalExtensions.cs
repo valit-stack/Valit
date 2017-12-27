c4a948c03c4328b5a025063d1651373ffb178831
Dariusz Pawlukiewicz <foreverframenet@gmail.com>
2017-12-27 17:49:45 +0100

    Added IsDefault flag in Error. Added missing default messages on all rules

--- src/Valit/ValitRuleDecimalExtensions.cs
+++ src/Valit/ValitRuleDecimalExtensions.cs
@@ -1,87 +1,89 @@
+using Valit.Errors;
+
 namespace Valit
 {
     public static class ValitRuleDecimalExtensions
     {
         public static IValitRule<TObject, decimal> IsGreaterThan<TObject>(this IValitRule<TObject, decimal> rule, decimal value) where TObject : class
-            => rule.Satisfies(p => p > value);
+            => rule.Satisfies(p => p > value).WithDefaultMessage(ErrorMessages.IsGreaterThan, value);
 
         public static IValitRule<TObject, decimal> IsGreaterThan<TObject>(this IValitRule<TObject, decimal> rule, decimal? value) where TObject : class
-            => rule.Satisfies(p => value.HasValue && p > value.Value);
+            => rule.Satisfies(p => value.HasValue && p > value.Value).WithDefaultMessage(ErrorMessages.IsGreaterThan, value);
 
         public static IValitRule<TObject, decimal?> IsGreaterThan<TObject>(this IValitRule<TObject, decimal?> rule, decimal value) where TObject : class
-            => rule.Satisfies(p => p.HasValue && p.Value > value);
+            => rule.Satisfies(p => p.HasValue && p.Value > value).WithDefaultMessage(ErrorMessages.IsGreaterThan, value);
 
         public static IValitRule<TObject, decimal?> IsGreaterThan<TObject>(this IValitRule<TObject, decimal?> rule, decimal? value) where TObject : class
-            => rule.Satisfies(p => p.HasValue && value.HasValue && p.Value > value.Value);
+            => rule.Satisfies(p => p.HasValue && value.HasValue && p.Value > value.Value).WithDefaultMessage(ErrorMessages.IsGreaterThan, value);
 
         public static IValitRule<TObject, decimal> IsLessThan<TObject>(this IValitRule<TObject, decimal> rule, decimal value) where TObject : class
-            => rule.Satisfies(p => p < value);
+            => rule.Satisfies(p => p < value).WithDefaultMessage(ErrorMessages.IsLessThan, value);
 
         public static IValitRule<TObject, decimal> IsLessThan<TObject>(this IValitRule<TObject, decimal> rule, decimal? value) where TObject : class
-            => rule.Satisfies(p => value.HasValue && p < value.Value);
+            => rule.Satisfies(p => value.HasValue && p < value.Value).WithDefaultMessage(ErrorMessages.IsLessThan, value);
 
         public static IValitRule<TObject, decimal?> IsLessThan<TObject>(this IValitRule<TObject, decimal?> rule, decimal value) where TObject : class
-            => rule.Satisfies(p => p.HasValue && p.Value < value);
+            => rule.Satisfies(p => p.HasValue && p.Value < value).WithDefaultMessage(ErrorMessages.IsLessThan, value);
 
         public static IValitRule<TObject, decimal?> IsLessThan<TObject>(this IValitRule<TObject, decimal?> rule, decimal? value) where TObject : class
-            => rule.Satisfies(p => p.HasValue && value.HasValue && p.Value < value.Value);
+            => rule.Satisfies(p => p.HasValue && value.HasValue && p.Value < value.Value).WithDefaultMessage(ErrorMessages.IsLessThan, value);
 
         public static IValitRule<TObject, decimal> IsGreaterThanOrEqualTo<TObject>(this IValitRule<TObject, decimal> rule, decimal value) where TObject : class
-            => rule.Satisfies(p => p >= value);
+            => rule.Satisfies(p => p >= value).WithDefaultMessage(ErrorMessages.IsGreaterThanOrEqualTo, value);
 
         public static IValitRule<TObject, decimal> IsGreaterThanOrEqualTo<TObject>(this IValitRule<TObject, decimal> rule, decimal? value) where TObject : class
-            => rule.Satisfies(p => value.HasValue && p >= value.Value);
+            => rule.Satisfies(p => value.HasValue && p >= value.Value).WithDefaultMessage(ErrorMessages.IsGreaterThanOrEqualTo, value);
 
         public static IValitRule<TObject, decimal?> IsGreaterThanOrEqualTo<TObject>(this IValitRule<TObject, decimal?> rule, decimal value) where TObject : class
-            => rule.Satisfies(p => p.HasValue && p.Value >= value);
+            => rule.Satisfies(p => p.HasValue && p.Value >= value).WithDefaultMessage(ErrorMessages.IsGreaterThanOrEqualTo, value);
 
         public static IValitRule<TObject, decimal?> IsGreaterThanOrEqualTo<TObject>(this IValitRule<TObject, decimal?> rule, decimal? value) where TObject : class
-            => rule.Satisfies(p => p.HasValue && value.HasValue && p.Value >= value.Value);
+            => rule.Satisfies(p => p.HasValue && value.HasValue && p.Value >= value.Value).WithDefaultMessage(ErrorMessages.IsGreaterThanOrEqualTo, value);
 
         public static IValitRule<TObject, decimal> IsLessThanOrEqualTo<TObject>(this IValitRule<TObject, decimal> rule, decimal value) where TObject : class
-            => rule.Satisfies(p => p <= value);
+            => rule.Satisfies(p => p <= value).WithDefaultMessage(ErrorMessages.IsLessThanOrEqualTo, value);
 
         public static IValitRule<TObject, decimal> IsLessThanOrEqualTo<TObject>(this IValitRule<TObject, decimal> rule, decimal? value) where TObject : class
-            => rule.Satisfies(p => value.HasValue && p <= value.Value);
+            => rule.Satisfies(p => value.HasValue && p <= value.Value).WithDefaultMessage(ErrorMessages.IsLessThanOrEqualTo, value);
 
         public static IValitRule<TObject, decimal?> IsLessThanOrEqualTo<TObject>(this IValitRule<TObject, decimal?> rule, decimal value) where TObject : class
-            => rule.Satisfies(p => p.HasValue && p.Value <= value);
+            => rule.Satisfies(p => p.HasValue && p.Value <= value).WithDefaultMessage(ErrorMessages.IsLessThanOrEqualTo, value);
 
         public static IValitRule<TObject, decimal?> IsLessThanOrEqualTo<TObject>(this IValitRule<TObject, decimal?> rule, decimal? value) where TObject : class
-            => rule.Satisfies(p => p.HasValue && value.HasValue && p.Value <= value.Value);
+            => rule.Satisfies(p => p.HasValue && value.HasValue && p.Value <= value.Value).WithDefaultMessage(ErrorMessages.IsLessThanOrEqualTo, value);
 
         public static IValitRule<TObject, decimal> IsEqualTo<TObject>(this IValitRule<TObject, decimal> rule, decimal value) where TObject : class
-            => rule.Satisfies(p => p == value);
+            => rule.Satisfies(p => p == value).WithDefaultMessage(ErrorMessages.IsEqualTo, value);
 
         public static IValitRule<TObject, decimal> IsEqualTo<TObject>(this IValitRule<TObject, decimal> rule, decimal? value) where TObject : class
-            => rule.Satisfies(p => value.HasValue && p == value.Value);
+            => rule.Satisfies(p => value.HasValue && p == value.Value).WithDefaultMessage(ErrorMessages.IsEqualTo, value);
 
         public static IValitRule<TObject, decimal?> IsEqualTo<TObject>(this IValitRule<TObject, decimal?> rule, decimal value) where TObject : class
-            => rule.Satisfies(p => p.HasValue && p.Value == value);
+            => rule.Satisfies(p => p.HasValue && p.Value == value).WithDefaultMessage(ErrorMessages.IsEqualTo, value);
 
         public static IValitRule<TObject, decimal?> IsEqualTo<TObject>(this IValitRule<TObject, decimal?> rule, decimal? value) where TObject : class
-            => rule.Satisfies(p => p.HasValue && value.HasValue && p.Value == value.Value);
+            => rule.Satisfies(p => p.HasValue && value.HasValue && p.Value == value.Value).WithDefaultMessage(ErrorMessages.IsEqualTo, value);
 
         public static IValitRule<TObject, decimal> IsPositive<TObject>(this IValitRule<TObject, decimal> rule) where TObject : class
-            => rule.Satisfies(p => p > 0m);
+            => rule.Satisfies(p => p > 0m).WithDefaultMessage(ErrorMessages.IsPositive);
 
         public static IValitRule<TObject, decimal?> IsPositive<TObject>(this IValitRule<TObject, decimal?> rule) where TObject : class
-            => rule.Satisfies(p => p.HasValue && p.Value > 0m);
+            => rule.Satisfies(p => p.HasValue && p.Value > 0m).WithDefaultMessage(ErrorMessages.IsPositive);
 
         public static IValitRule<TObject, decimal> IsNegative<TObject>(this IValitRule<TObject, decimal> rule) where TObject : class
-            => rule.Satisfies(p => p < 0m);
+            => rule.Satisfies(p => p < 0m).WithDefaultMessage(ErrorMessages.IsNegative);
 
         public static IValitRule<TObject, decimal?> IsNegative<TObject>(this IValitRule<TObject, decimal?> rule) where TObject : class
-            => rule.Satisfies(p => p.HasValue && p.Value < 0m);
+            => rule.Satisfies(p => p.HasValue && p.Value < 0m).WithDefaultMessage(ErrorMessages.IsNegative);
 
         public static IValitRule<TObject, decimal> IsNonZero<TObject>(this IValitRule<TObject, decimal> rule) where TObject : class
-            => rule.Satisfies(p => p != 0m);
+            => rule.Satisfies(p => p != 0m).WithDefaultMessage(ErrorMessages.IsNonZero);
 
         public static IValitRule<TObject, decimal?> IsNonZero<TObject>(this IValitRule<TObject, decimal?> rule) where TObject : class
-            => rule.Satisfies(p => p.HasValue && p.Value != 0m);
+            => rule.Satisfies(p => p.HasValue && p.Value != 0m).WithDefaultMessage(ErrorMessages.IsNonZero);
 
         public static IValitRule<TObject, decimal?> Required<TObject>(this IValitRule<TObject, decimal?> rule) where TObject : class
-            => rule.Satisfies(p => p.HasValue);
+            => rule.Satisfies(p => p.HasValue).WithDefaultMessage(ErrorMessages.Required);
 
     }
 }
--- src/Valit/ValitRuleUInt32Extensions.cs
+++ src/Valit/ValitRuleUInt32Extensions.cs
@@ -1,84 +1,86 @@
+using Valit.Errors;
+
 namespace Valit
 {
     public static class ValitRuleUInt32Extensions
     {
         public static IValitRule<TObject, uint> IsGreaterThan<TObject>(this IValitRule<TObject, uint> rule, uint value) where TObject : class
-            => rule.Satisfies(p => p > value);
+            => rule.Satisfies(p => p > value).WithDefaultMessage(ErrorMessages.IsGreaterThan, value);
 
         public static IValitRule<TObject, uint> IsGreaterThan<TObject>(this IValitRule<TObject, uint> rule, uint? value) where TObject : class
-            => rule.Satisfies(p => value.HasValue && p > value.Value);
+            => rule.Satisfies(p => value.HasValue && p > value.Value).WithDefaultMessage(ErrorMessages.IsGreaterThan, value);
 
 
         public static IValitRule<TObject, uint?> IsGreaterThan<TObject>(this IValitRule<TObject, uint?> rule, uint value) where TObject : class
-            => rule.Satisfies(p => p.HasValue && p.Value > value);
+            => rule.Satisfies(p => p.HasValue && p.Value > value).WithDefaultMessage(ErrorMessages.IsGreaterThan, value);
 
         public static IValitRule<TObject, uint?> IsGreaterThan<TObject>(this IValitRule<TObject, uint?> rule, uint? value) where TObject : class
-            => rule.Satisfies(p => p.HasValue && value.HasValue && p.Value > value.Value);
+            => rule.Satisfies(p => p.HasValue && value.HasValue && p.Value > value.Value).WithDefaultMessage(ErrorMessages.IsGreaterThan, value);
 
 
         public static IValitRule<TObject, uint> IsLessThan<TObject>(this IValitRule<TObject, uint> rule, uint value) where TObject : class
-            => rule.Satisfies(p => p < value);
+            => rule.Satisfies(p => p < value).WithDefaultMessage(ErrorMessages.IsLessThan, value);
 
         public static IValitRule<TObject, uint> IsLessThan<TObject>(this IValitRule<TObject, uint> rule, uint? value) where TObject : class
-            => rule.Satisfies(p => value.HasValue && p < value.Value);
+            => rule.Satisfies(p => value.HasValue && p < value.Value).WithDefaultMessage(ErrorMessages.IsLessThan, value);
 
 
         public static IValitRule<TObject, uint?> IsLessThan<TObject>(this IValitRule<TObject, uint?> rule, uint value) where TObject : class
-            => rule.Satisfies(p => p.HasValue && p.Value < value);
+            => rule.Satisfies(p => p.HasValue && p.Value < value).WithDefaultMessage(ErrorMessages.IsLessThan, value);
 
         public static IValitRule<TObject, uint?> IsLessThan<TObject>(this IValitRule<TObject, uint?> rule, uint? value) where TObject : class
-            => rule.Satisfies(p => p.HasValue && value.HasValue && p.Value < value.Value);
+            => rule.Satisfies(p => p.HasValue && value.HasValue && p.Value < value.Value).WithDefaultMessage(ErrorMessages.IsLessThan, value);
 
 
         public static IValitRule<TObject, uint> IsGreaterThanOrEqualTo<TObject>(this IValitRule<TObject, uint> rule, uint value) where TObject : class
-            => rule.Satisfies(p => p >= value);
+            => rule.Satisfies(p => p >= value).WithDefaultMessage(ErrorMessages.IsGreaterThanOrEqualTo, value);
 
         public static IValitRule<TObject, uint> IsGreaterThanOrEqualTo<TObject>(this IValitRule<TObject, uint> rule, uint? value) where TObject : class
-            => rule.Satisfies(p => value.HasValue && p >= value.Value);
+            => rule.Satisfies(p => value.HasValue && p >= value.Value).WithDefaultMessage(ErrorMessages.IsGreaterThanOrEqualTo, value);
 
 
         public static IValitRule<TObject, uint?> IsGreaterThanOrEqualTo<TObject>(this IValitRule<TObject, uint?> rule, uint value) where TObject : class
-            => rule.Satisfies(p => p.HasValue && p.Value >= value);
+            => rule.Satisfies(p => p.HasValue && p.Value >= value).WithDefaultMessage(ErrorMessages.IsGreaterThanOrEqualTo, value);
 
         public static IValitRule<TObject, uint?> IsGreaterThanOrEqualTo<TObject>(this IValitRule<TObject, uint?> rule, uint? value) where TObject : class
-            => rule.Satisfies(p => p.HasValue && value.HasValue && p.Value >= value.Value);
+            => rule.Satisfies(p => p.HasValue && value.HasValue && p.Value >= value.Value).WithDefaultMessage(ErrorMessages.IsGreaterThanOrEqualTo, value);
 
 
         public static IValitRule<TObject, uint> IsLessThanOrEqualTo<TObject>(this IValitRule<TObject, uint> rule, uint value) where TObject : class
-            => rule.Satisfies(p => p <= value);
+            => rule.Satisfies(p => p <= value).WithDefaultMessage(ErrorMessages.IsLessThanOrEqualTo, value);
 
         public static IValitRule<TObject, uint> IsLessThanOrEqualTo<TObject>(this IValitRule<TObject, uint> rule, uint? value) where TObject : class
-            => rule.Satisfies(p => value.HasValue && p <= value.Value);
+            => rule.Satisfies(p => value.HasValue && p <= value.Value).WithDefaultMessage(ErrorMessages.IsLessThanOrEqualTo, value);
 
 
         public static IValitRule<TObject, uint?> IsLessThanOrEqualTo<TObject>(this IValitRule<TObject, uint?> rule, uint value) where TObject : class
-            => rule.Satisfies(p => p.HasValue && p.Value <= value);
+            => rule.Satisfies(p => p.HasValue && p.Value <= value).WithDefaultMessage(ErrorMessages.IsLessThanOrEqualTo, value);
 
         public static IValitRule<TObject, uint?> IsLessThanOrEqualTo<TObject>(this IValitRule<TObject, uint?> rule, uint? value) where TObject : class
-            => rule.Satisfies(p => p.HasValue && value.HasValue && p.Value <= value.Value);
+            => rule.Satisfies(p => p.HasValue && value.HasValue && p.Value <= value.Value).WithDefaultMessage(ErrorMessages.IsLessThanOrEqualTo, value);
 
 
         public static IValitRule<TObject, uint> IsEqualTo<TObject>(this IValitRule<TObject, uint> rule, uint value) where TObject : class
-            => rule.Satisfies(p => p == value);
+            => rule.Satisfies(p => p == value).WithDefaultMessage(ErrorMessages.IsEqualTo, value);
 
         public static IValitRule<TObject, uint> IsEqualTo<TObject>(this IValitRule<TObject, uint> rule, uint? value) where TObject : class
-            => rule.Satisfies(p => value.HasValue && p == value.Value);
+            => rule.Satisfies(p => value.HasValue && p == value.Value).WithDefaultMessage(ErrorMessages.IsEqualTo, value);
 
 
         public static IValitRule<TObject, uint?> IsEqualTo<TObject>(this IValitRule<TObject, uint?> rule, uint value) where TObject : class
-            => rule.Satisfies(p => p.HasValue && p.Value == value);
+            => rule.Satisfies(p => p.HasValue && p.Value == value).WithDefaultMessage(ErrorMessages.IsEqualTo, value);
 
         public static IValitRule<TObject, uint?> IsEqualTo<TObject>(this IValitRule<TObject, uint?> rule, uint? value) where TObject : class
-            => rule.Satisfies(p => p.HasValue && value.HasValue && p.Value == value.Value);
+            => rule.Satisfies(p => p.HasValue && value.HasValue && p.Value == value.Value).WithDefaultMessage(ErrorMessages.IsEqualTo, value);
 
 
         public static IValitRule<TObject, uint> IsNonZero<TObject>(this IValitRule<TObject, uint> rule) where TObject : class
-            => rule.Satisfies(p => p != 0u);
+            => rule.Satisfies(p => p != 0u).WithDefaultMessage(ErrorMessages.IsNonZero);
 
         public static IValitRule<TObject, uint?> IsNonZero<TObject>(this IValitRule<TObject, uint?> rule) where TObject : class
-            => rule.Satisfies(p => p.HasValue && p.Value != 0u);
+            => rule.Satisfies(p => p.HasValue && p.Value != 0u).WithDefaultMessage(ErrorMessages.IsNonZero);
 
         public static IValitRule<TObject, uint?> Required<TObject>(this IValitRule<TObject, uint?> rule) where TObject : class
-            => rule.Satisfies(p => p.HasValue);
+            => rule.Satisfies(p => p.HasValue).WithDefaultMessage(ErrorMessages.Required);
     }
 }
