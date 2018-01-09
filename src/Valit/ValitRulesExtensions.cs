bd213f51c104d31189028c1bfa80e04c5929dc77
tim <timdeschryver@outlook.com>
2018-01-09 18:50:25 +0100

    rename to ValitRulesExtensions

--- src/Valit/ValitRulesExtensions.cs
+++ src/Valit/ValitRulesExtensions.cs
@@ -3,7 +3,7 @@ using Valit.Validators;
 
 namespace Valit
 {
-    public static class ValitRulesProviderExtensions
+    public static class ValitRulesExtensions
     {
         public static IValitator<TObject> CreateValitator<TObject>(this IValitRules<TObject> valitRules) where TObject : class
         {
